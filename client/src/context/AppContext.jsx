f2a6b336275756dda9ad6ed1bd22c65f331a8237
anshul_c0des <anshuldhariya5@gmail.com>
2025-05-03 18:47:21 +0530

    First Commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, use, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+
+    const currency = import.meta.VITE_CURRENCY;
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([])
+    const [cartItems, setCartItems] = useState({})
+
+    const fetchProducts = async() =>{  //fetch products
+        setProducts(dummyProducts)
+    }
+    const addToCart = () =>{  //add items to cart
+        let cartData = structuredClone(cartItems)
+        if(cartData[itemId]){
+            cartData[itemId]++;
+        }else{
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Added to cart")
+    }
+    const updateCartItems = (itemId, quantity)=>{  //update quantity in cart
+        let cartData = structuredClone(cartItems);
+    }
+
+    useEffect(() => {
+      return () => {
+        fetchProducts
+      };
+    }, [])
+
+    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency}
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = ()=>{
+    return useContext(AppContext);
+}

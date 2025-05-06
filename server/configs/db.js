369959de3a2f020f5fe5319bda8cce881c4509bf
anshul_c0des <anshuldhariya5@gmail.com>
2025-05-06 15:19:54 +0530

    configured backend, created user schema

---
+++ server/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async() =>{
+    try {
+        mongoose.connection.on('connected', ()=>console.log('DataBase Connected')
+        );
+        await mongoose.connect(`${process.env.MONGODB_URI}/greenbasket`)
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config'
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+await connectDB();
+
+//Allowed Origins
+const allowedOrigins = ['http://localhost:5174']
+
+//Middleware Confg
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
+
+app.get('/', (req, res)=> res.send('API Working'));
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
